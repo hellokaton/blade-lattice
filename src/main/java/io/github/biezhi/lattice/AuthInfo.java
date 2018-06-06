a79e81d3ba7a21eb28a47ec0aea2766dc064ee6e
biezhi <biezhi.me@gmail.com>
2018-06-06 13:41:25 +0800

    :sparkles: add hasPermission and hasRole method

--- src/main/java/io/github/biezhi/lattice/AuthInfo.java
+++ src/main/java/io/github/biezhi/lattice/AuthInfo.java
@@ -18,4 +18,18 @@ public class AuthInfo<T> {
     private Set<String> roles;
     private Set<String> permissions;
 
+    public boolean hasPermission(String permission) {
+        if (null != permissions) {
+            return permissions.contains(permission);
+        }
+        return false;
+    }
+
+    public boolean hasRole(String role) {
+        if (null != roles) {
+            return roles.contains(role);
+        }
+        return false;
+    }
+
 }
